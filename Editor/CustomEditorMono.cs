01be9c6a7d573f70f0bbd33cb10e54022a0abb7c
KamilTheFox <100517471+KamilTheFox@users.noreply.github.com>
2024-12-24 17:58:46 +0300

    FixBagBuild

--- Editor/CustomEditorMono.cs
+++ Editor/CustomEditorMono.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor;
 
 namespace VulpesTool.Editor
 {
-    internal class CustomEditorMono : CustomGUIEditor
+    [CustomEditor(typeof(VulpesMonoBehaviour), true), CanEditMultipleObjects]
+    public class CustomEditorMono : CustomGUIEditor
     {
+
     }
 }
